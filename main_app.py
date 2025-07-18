46d3e6e17cf93925f53d7f6296f5c1640b86d8a6
Azizah2460340 <zahputzah.08@gmail.com>
2025-07-18 07:55:57 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1127,7 +1127,7 @@ def show_rating():
 
     # Pesan apresiasi/mohon maaf langsung di bawah rating
     if rating >= 4:
-        st.success("Terima kasih atas rating tinggi kamu! Kamu luar biasa! 🥰⭐️
+        st.success("Terima kasih atas rating tinggi kamu! Kamu luar biasa! 🥰⭐️")
          st.info("Dukunganmu sangat berarti! Semoga O-KimiaKu makin bermanfaat 🎉")
     elif rating <= 3:
         st.error("Kami mohon maaf atas ketidaknyamananmu 😔🙏")
