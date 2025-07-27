2c78cc29e3c96ed6a451353e0113bbdeb44181fa
Azizah2460340 <zahputzah.08@gmail.com>
2025-07-27 19:35:49 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -887,9 +887,7 @@ def show_karbohidrat():
 def show_chatbot():
     st.title("💬 Chatbot O-KIMIAKU")
     question = st.text_input("Tanyakan sesuatu tentang senyawa kimia (misal: kepolaran ester):", key="chat_input")
-    if st.button("Kirim"):
-        else:
-            st.warning("Silakan masukkan pertanyaan dulu.")
+    st.button("Kirim"):
 
     if question:
         q = question.lower()
